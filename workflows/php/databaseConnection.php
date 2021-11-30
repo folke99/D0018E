0f86bbb95723bfe473be105f2813a1b032cc5b5c
Reqtiade <j.florberg@gmail.com>
2021-11-30 10:22:04 +0100

    incement quantity when adding more of the same item to cart

--- workflows/php/databaseConnection.php
+++ workflows/php/databaseConnection.php
@@ -1,9 +1,16 @@
 <!-- dbconnection.php -->
 <?php
+/*
 $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbName     = "d0018e";
+*/
+
+$servername = "utbweb.its.ltu.se";
+$username   = "19980724";
+$password   = "19980724";
+$dbName     = "db19980724";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbName);
