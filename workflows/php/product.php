502150ca516efcd567f3fd341d8a28816a19cec0
folke99 <folkefd@gmail.com>
2021-12-14 09:38:05 +0100

    fixed so you can only rate 1-5

--- workflows/php/product.php
+++ workflows/php/product.php
@@ -133,7 +133,14 @@ echo <<<HTML
     
             <div class="rRating">
               <label for="rRating"><h2>Rate</h2></label>
-              <input type="text" placeholder="Enter Rate" name="rRating" required>
+
+              <select name="rRating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
             </div>
     
             <input type="hidden" name="pID" value="$pID">
